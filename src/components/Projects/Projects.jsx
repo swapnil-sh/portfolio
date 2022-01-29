bdb425792e6c400046fda4a5cc324ae9d1e37ff0
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-01-29 14:05:26 +0530

    Designed Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import {projectsData} from './../../assets/projectsData';
 import FooterLink from "../FooterLink/FooterLink";
 import Header from "../Header/Header";
 import './Projects.css';
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
     return (
@@ -11,6 +13,25 @@ const Projects = () => {
                 details="I have always loved learning new technologies and building something out of it. Below are some my cool projects that Ive worked on!"    
             />
             
+            <div className="project-cards-container">
+                {
+                    projectsData.map(
+                    ({
+                        projectName,
+                        projectDescription,
+                        imageUrl,
+                        videoUrl,
+                        projectUrl
+                    }) => {
+                        return (
+                                <ProjectCard 
+                                    projectName={projectName} 
+                                    projectDescription={projectDescription} 
+                                    imageUrl={imageUrl} videoUrl={videoUrl} 
+                                    projectUrl={projectUrl} />
+                        )})}
+            </div>
+
             <FooterLink 
                 phrase="Checkout my " 
                 link="skills!" 
